7cd58b2e2a041181c3cdb23e4455a2098455645b
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-01-31 18:30:19 +0100

    WIP Matrix bridge

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+#   This file is part of Transponder.
+#
+#   Matrix Python SDK is licensed under Apache License 2.0 see their repo for
+#   more information. The Transponder bridge is also licensed under Apache
+#   License 2.0
+
+"""
+Transponder is a Sailfish OS application that provide access to different 
+messaging providers using Python plugins.
+"""
+
+__version__ = "0.0.1"
